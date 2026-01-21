4be0366e9bb41e8a08d955d05e39d61ffbbd99da
marianagopas <khodorovskamarjana@gmail.com>
2026-01-21 02:26:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Канал для прослуховування
-CHANNEL_USERNAME = "Gopaska_boutique"
+CHANNEL_USERNAME = "Gopaska_boutique_Italyclothing"
 
 # Фото старше скількох днів ігнорувати
 MAX_AGE_DAYS = 35  # 5 тижнів
