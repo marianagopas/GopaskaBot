32cb25d4d3189f18a66fcd07604efc3bad912ace
marianagopas <khodorovskamarjana@gmail.com>
2026-01-21 02:13:30 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+# Токен Telegram бота
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Ключ OpenAI
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Канал для прослуховування
+CHANNEL_USERNAME = "Gopaska_boutique"
+
+# Фото старше скількох днів ігнорувати
+MAX_AGE_DAYS = 35  # 5 тижнів
